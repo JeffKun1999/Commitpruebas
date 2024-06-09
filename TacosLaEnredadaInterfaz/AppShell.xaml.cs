933d86aff43c141bcd63adb9dd8aaba8d63bb3b6
JeffKun1999 <jefferson.ruiz.pico@udla.edu.ec>
2024-06-08 21:13:04 -0500

    Prueba dos

---
+++ TacosLaEnredadaInterfaz/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TacosLaEnredadaInterfaz
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
